1b6403b7be322ca133720647203f747adb43199b
André Flávio de Oliveira <141851943+andreflavio@users.noreply.github.com>
2024-04-30 11:37:56 -0300

    Add files via upload

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,10 @@
 // components/Menu/index.tsx
 import React from 'react';
 import { useMatch, Link as NavLink } from 'react-router-dom';
+import '../../styles.css'
+
+
+
 
 
 const Menu: React.FC = () => {
@@ -8,11 +12,14 @@ const Menu: React.FC = () => {
     <div className="menu">
       <ul>
         <li><NavLink to="/timemania" className={useMatch("/timemania") ? 'active' : ''}>Timemania</NavLink></li>
-        <li><NavLink to="/megasena" className={useMatch("/megasena") ? 'active' : ''}>Mega Sena</NavLink></li>
+        <li><NavLink to="/megasena" className={useMatch("/megasena") ? 'active megasena' : 'megasena'}>Mega Sena</NavLink></li>
         <li><NavLink to="/quina" className={useMatch("/quina") ? 'active' : ''}>Quina</NavLink></li>
       </ul>
     </div>
   );
 };
 
+
 export default Menu;
+
+
